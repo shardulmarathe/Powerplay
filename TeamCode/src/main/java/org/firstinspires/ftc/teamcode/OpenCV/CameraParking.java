cff280b408073977e593e52b77fc04ed0ce8eaa1
UAgarwal7 <utsavagar7@gmail.com>
2023-01-25 21:18:26 -0800

    Started new Hardware class and Camera with Parking code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/CameraParking.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.OpenCV;
+
+public class CameraParking {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/HardwareWCamera.java
@@ -0,0 +1,179 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.OpenCV;
+
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+package org.firstinspires.ftc.teamcode.OpenCV;
+
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfDouble;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfPoint3f;
+import org.opencv.core.Point;
+import org.opencv.core.Point3;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.apriltag.AprilTagDetectorJNI;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a Pushbot.
+ * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ * Motor channel:  Left  drive motor:        "left_drive"
+ * Motor channel:  Right drive motor:        "right_drive"
+ * Motor channel:  Manipulator drive motor:  "left_arm"
+ * Servo channel:  Servo to open left claw:  "left_hand"
+ * Servo channel:  Servo to open right claw: "right_hand"
+ */
+public class HardwareWCamera
+{
+    /* Public OpMode members. */
+    public DcMotor  leftfront = null;
+    public DcMotor  rightfront = null;
+    public DcMotor  leftback = null;
+    public DcMotor  rightback = null;
+    public DcMotor  Lift = null;
+    ////    public DcMotor  wobblegoalarm = null;
+////    public DcMotor  shooterone = null;
+////    public DcMotor  Wheelintake = null;
+////    public Servo Launcher = null;
+////    public Servo rampservo = null;
+    public Servo grabber = null;
+    public Servo grabber2 = null;
+
+
+
+
+
+
+
+
+    /* local OpMode members. */
+    HardwareMap hwMap = null;
+    private ElapsedTime period  = new ElapsedTime();
+
+
+
+    /* Constructor */
+    public HardwareWCamera(){
+
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap hwMap) {
+        // Save reference to Hardware map
+        this.hwMap = hwMap;
+
+        // Define and Initialize Motors
+        leftfront = this.hwMap.get(DcMotor.class, "leftfront");
+        leftback = this.hwMap.get(DcMotor.class, "leftback");
+        rightfront = this.hwMap.get(DcMotor.class, "rightfront");
+        rightback = this.hwMap.get(DcMotor.class, "rightback");
+        Lift = this.hwMap.get(DcMotor.class, "Lift");
+        grabber = this.hwMap.get(Servo.class,"grabber");
+        grabber2 = this.hwMap.get(Servo.class,"grabber2");
+
+
+
+        //extra = this.hwMap.get(Servo.class,"extra");
+
+
+
+        leftfront.setDirection(DcMotor.Direction.FORWARD);
+        rightfront.setDirection(DcMotor.Direction.REVERSE);
+        leftback.setDirection(DcMotor.Direction.FORWARD);
+        rightback.setDirection(DcMotor.Direction.REVERSE);
+
+        Lift.setDirection(DcMotor.Direction.FORWARD);
+//        wobblegoalarm.setDirection(DcMotor.Direction.FORWARD);
+//        shooterone.setDirection(DcMotor.Direction.FORWARD);
+//        Wheelintake.setDirection(DcMotor.Direction.REVERSE);
+//        Launcher.setPosition(0.1);
+        grabber.setPosition(0.6);
+        grabber2.setPosition(0.4);
+
+
+
+        //extra.setPosition(1.0);
+
+
+
+
+
+
+
+
+
+
+        // Set all motors to zero power
+        leftfront.setPower(0.0);
+        rightfront.setPower(0.0);
+        leftback.setPower(0.0);
+        rightback.setPower(0.0);
+        Lift.setPower(0.0);
+
+
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftfront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightfront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftback.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightback.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+
+
+    }
+
+
+
+}
+
